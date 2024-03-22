718b93304dc291daae18ca819b0bd2379e1b4097
gajananbhagt2000 <98050634+gajananbhagt2000@users.noreply.github.com>
2024-03-22 22:19:44 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+
+import React from 'react';
+import Menu from 'C:\Gajanan\React\my-app\src\Components\Menu.js';
+import About from './Components/About';
+import Footer from './Components/Footer';
+
+function App() {
+  return (
+    <div>
+      <Menu />
+      <About />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
